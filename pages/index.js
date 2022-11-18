f11090c3fb82a392408b393b98a07b5e5b5b41f8
Blessing Okonkwo <theblessingokonkwo@gmail.com>
2022-11-18 14:41:13 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
     return (
       <>
         <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+          {/* <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script> */}
         </Head>
         <article>
         <h1>{date}</h1>
